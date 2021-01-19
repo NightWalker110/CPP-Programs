52a79ef6d9080d8c684cef9bb21e72e42c29135b
Shubham Kumar Singh <61842432+NightWalker110@users.noreply.github.com>
2021-01-19 23:52:43 +0530

    Create Swapping.cpp

---
+++ Swapping.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float a, b;
+		cout << "Enter first number: ";
+		cin >> a ;
+		cout << "Enter second number: ";
+		cin >> b;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		cout << "We have swapped your numbers, result in below." << endl;
+		cout << "a = " << a << endl;
+		cout << "b = " << b << endl;
+	system("pause");
+	return 0;
+}
